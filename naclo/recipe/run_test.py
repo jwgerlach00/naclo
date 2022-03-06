d45f24de33cba0227841850fb287a00ca9e6b13e
jwgerlach00 <jwgerlach00@gmail.com>
2022-03-06 12:38:37 -0500

    fixing run_test

--- naclo/recipe/run_test.py
+++ naclo/recipe/run_test.py
@@ -2,8 +2,8 @@
 # import os
 
 # os.chdir(os.path.join(os.getcwd(), '__unit_tests'))
-import fragments_test
-import __unit_tests
-# from __unit_tests import fragments_test, mol_conversion_test, mol_stats_test, neutralization_test
+# import fragments_test
+# import __unit_tests
+from __unit_tests import fragments_test, mol_conversion_test, mol_stats_test, neutralization_test
 # from __unit_tests.df_ops_test import df_ops_test
 
