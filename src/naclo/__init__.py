b583ad7a876e431f29e4163fb1510ee5d563509c
jacob_personal <jwgerlach00@gmail.com>
2022-06-28 11:25:13 -0400

    Changes to default params import because it was fucked

--- src/naclo/__init__.py
+++ src/naclo/__init__.py
@@ -2,8 +2,6 @@ __doc__ == """
 **naclo** is a Python cleaning toolset for small molecule drug discovery datasets.
 """
 
-
-from turtle import clear
 from naclo.mol_stats import *
 from naclo.mol_conversion import *
 from naclo import database
@@ -13,12 +11,4 @@ from naclo import neutralize
 from naclo import rdpickle
 from naclo import visualization
 from naclo.Bleach import Bleach
-
-import json
-
-with open('naclo/assets/default_options.json') as f:
-    bleach_default_options = json.load(f)
-    
-with open('naclo/assets/default_params.json') as f:
-
-    bleach_default_params = json.load(f)
+from naclo.assets import bleach_default_options, bleach_default_params
