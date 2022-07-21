0d004b4acfd663ba4d1169611359c84d24f56e46
jacob_personal <jwgerlach00@gmail.com>
2022-07-21 12:55:30 -0400

    Version 0.1.82 -> pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='naclo',
-    version='0.1.80',
+    version='0.1.82',
     license='MIT',
     author='Jacob Gerlach',
     author_email='jwgerlach00@gmail.com',
